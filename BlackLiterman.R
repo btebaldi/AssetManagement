3be3eefa4558301722d68cba1c329a0d9d3353ac
Bruno Tebaldi Q Barbosa <btebaldi@hotmail.com>
2018-10-09 18:08:54 -0300

    BL

--- BlackLiterman.R
+++ BlackLiterman.R
@@ -1,12 +1,19 @@
+# Limpa variaveis
+rm(list = ls())
 
-
-AssetClass=c('US Bonds', 'Int Bonds', 'US Large Growth', 'US Large Value', 
+#  carrega livrarias
+library(readxl)
+AssetNames=c('US Bonds', 'Int Bonds', 'US Large Growth', 'US Large Value', 
              'US Small Growth', 'US Small Value', 'Int Dev Equity',
              'Int Emerg Equity')
-ret_Hist=c(3.15, 1.75,-6.39,-2.86, -6.75,-0.54, -6.75, -5.26)
-ret_CAPM_GSMI=c(0.02, 0.18, 5.57, 3.39, 6.59, 3.16, 3.92, 5.60)
-ret_CAPM_Port=c(0.08, 0.67, 6.41, 4.08, 7.43, 3.70, 4.80, 6.60)
-eqVec=c(0.08, 0.67, 6.41, 4.08, 7.43, 3.70, 4.80, 6.60)
+
+ret <- read_excel("Data.xlsx", sheet = "Planilha2", 
+                   col_names = FALSE, range = "B1:E8" )
+rownames(ret) = AssetNames
+
+omega <- read_excel("Data.xlsx", sheet = "Sigma", 
+                   col_names = FALSE, range="B1:I8")
+colnames(omega) = AssetNames
 
 # Weights
 W_Sigma = matrix(data = NA, nrow = length(AssetClass), ncol=length(AssetClass))
